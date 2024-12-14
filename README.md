ccb0c59b3421e01a14ea6d65256c4b3f7e9265d7
farhan-ahmed1 <abdia8001@gmail.com>
2024-12-14 12:30:48 -0600

    added login page

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 A blogging web application using Python and Flask
 
 # How to Run
+Activate your virtual environment and run the Flask application again:
+source venv/bin/activate
 flask run
 
 # Possible Error
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
 
 @app.route('/')
+# Base route
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
@@ -16,3 +19,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+# Login Route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
