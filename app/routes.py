3db1d0fd164da4a69d25c33e06b4fe0351424def
farhan-ahmed1 <abdia8001@gmail.com>
2024-12-19 09:27:59 -0600

    editing profile functionality

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,7 @@ from urllib.parse import urlsplit
 from app import db
 from app.forms import RegistrationForm
 from datetime import datetime, timezone
+from app.forms import EditProfileForm
 
 @app.route('/')
 # Base route
@@ -84,3 +85,18 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.now(timezone.utc)
         db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
