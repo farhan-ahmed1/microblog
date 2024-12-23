8b6e8e63d0ff9f5ca81c05e93b6424b7da25ca41
farhan-ahmed1 <abdia8001@gmail.com>
2024-12-23 10:40:13 -0600

    display blog posts

--- app/routes.py
+++ app/routes.py
@@ -15,13 +15,20 @@ from app.forms import RegistrationForm
 from datetime import datetime, timezone
 from app.forms import EditProfileForm
 from app.forms import EmptyForm
+from app.forms import PostForm
+from app.models import Post
 
-@app.route('/')
-# Base route
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Miguel'}
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'John'},
@@ -32,7 +39,8 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template("index.html", title='Home Page', posts=posts)
+    posts = db.session.scalars(current_user.following_posts()).all()
+    return render_template("index.html", title='Home Page', form=form, posts=posts)
 # Login Route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
